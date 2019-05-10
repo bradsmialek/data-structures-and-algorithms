ed5199bad48b83b6335684d2e841cd05e2518552
bjwine0 <bradsmilek@gmail.com>
2019-05-10 16:41:18 -0700

    1-2 pass

--- code-challenges/regex-p2/challenge-11.test.js
+++ code-challenges/regex-p2/challenge-11.test.js
@@ -28,7 +28,8 @@ Note: if you ever need to validate an email using a regex in practice, the Inter
 ------------------------------------------------------------------------------------------------ */
 
 const validateEmail = (email) => {
-  // Solution code here...
+  let regex = /^\w+([.]?\w+)@\w+([.]?\w+)(?:.com|.net|.org)\b/g;
+  return regex.test(email);
 };
 
 /* ------------------------------------------------------------------------------------------------
