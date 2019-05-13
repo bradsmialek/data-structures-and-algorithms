89f6dbeaffba41664e8f41ce1aac251a92694ee6
bjwine0 <bradsmilek@gmail.com>
2019-05-13 13:42:48 -0700

    1-3 passing

--- code-challenges/two-dimensional-arrays/challenges-12.test.js
+++ code-challenges/two-dimensional-arrays/challenges-12.test.js
@@ -71,9 +71,7 @@ const errands = [
   }
 ];
 
-const howManyTreats = (arr) => {
-  // Solution code here...
-};
+const howManyTreats = arr => arr[2].items[1].quantity
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
