16cf9715f2a9f9581fe06ded0692373f2e7c0f8f
bjwine0 <bradsmilek@gmail.com>
2019-08-14 18:56:19 -0700

    changed files

--- code-challenges/Data-Structures/__tests__/tree.test.js
+++ code-challenges/Data-Structures/__tests__/tree.test.js
@@ -1,38 +1,102 @@
 'use strict';
 
-// Can successfully instantiate an empty tree
-// Can successfully instantiate a tree with a single root node
-// Can successfully add a left child and right child to a single root node
-// Can successfully return a collection from a preorder traversal
-// Can successfully return a collection from an inorder traversal
-// Can successfully return a collection from a postorder traversal
 
 'use strict';
-
-const multiBracket = require('../../multiBracketValidation/multi-bracket-validation.js');
-
-//Test should include;
-// “Happy Path” - Expected outcome
-// Expected failure
-// Edge Case (if applicable/obvious)
-
-describe('Queue with stacks tests', () => {
-  it('can enqueue things', () => {
-    let qs = new AnimalQ;
-    qs.enqueue(5);
-    qs.enqueue(10);
-    qs.enqueue(15);
-    qs.enqueue(20);
-    expect(qs.stack1.top.value).toBe(20)
+let BT = require('./tree.js').BinaryTree;
+let Node = require('./tree.js').Node;
+let two = new Node(2);
+let seven = new Node(7);
+let five = new Node(5);
+let six = new Node(6);
+let nine = new Node(9);
+let eleven = new Node(11);
+let four = new Node(4);
+describe('Tree tests', () => {
+  it('Can successfully instantiate an empty tree', () => {
+    let tree = new BT();
+    expect(tree).toEqual({ root: null });
   });
-
-  it('can dequeue things', () => {
-    let qs = new AnimalQ;
-    qs.enqueue(5);
-    qs.enqueue(10);
-    qs.enqueue(15);
-    qs.enqueue(20);
-    expect(qs.dequeue()).toBe(5)
+  it('Can successfully instantiate a tree with a single root node', () => {
+    let tree = new BT();
+    tree.root = two;
+    expect(tree.root.value).toEqual(2);
   });
-
+  it('Can successfully add a right child to a single root node', () => {
+    let tree = new BT();
+    tree.root = two;
+    tree.root.right = five;
+    expect(tree.root.right).toBeDefined();
+  });
+  it('Can successfully add a left child to a single root node', () => {
+    let tree = new BT();
+    tree.root = two;
+    tree.root.right = five;
+    tree.root.left = seven;
+    expect(tree.root.left).toBeDefined();
+  });
+  it('Can successfully return a collection from a preorder traversal', () => {
+    let tree = new BT();
+    tree.root = two;
+    tree.root.right = five;
+    tree.root.left = seven;
+    tree.root.left = two;
+    tree.root.right = six;
+    tree.root.right = nine;
+    tree.root.left = five;
+    tree.root.right = eleven;
+    tree.root.left = four;
+    expect(tree.preOrder([2, 7, 2, 6, 5, 11, 5, 9, 4])).tobes
+  });
+  it('Can successfully return a collection from a inorder traversal', () => {
+    let tree = new BT();
+    tree.root = two;
+    tree.root.right = five;
+    tree.root.left = seven;
+    tree.root.left = two;
+    tree.root.right = six;
+    tree.root.right = nine;
+    tree.root.left = five;
+    tree.root.right = eleven;
+    tree.root.left = four;
+    expect(tree.inOrder([2, 7, 5, 6, 11, 2, 5, 9, 4])).tobe
+  });
+  it('Can successfully return a collection from a postorder traversal', () => {
+    let tree = new BT();
+    tree.root = two;
+    tree.root.right = five;
+    tree.root.left = seven;
+    tree.root.left = two;
+    tree.root.right = six;
+    tree.root.right = nine;
+    tree.root.left = five;
+    tree.root.right = eleven;
+    tree.root.left = four;
+    expect(tree.postOrder([2, 5, 11, 6, 7, 4, 9, 5, 2])).tobe
+  });
+  it('can successfully return a collection from a breadth-first traversal', () => {
+    let tree = new BT();
+    tree.root = two;
+    tree.root.right = five;
+    tree.root.left = seven;
+    tree.root.left = two;
+    tree.root.right = six;
+    tree.root.right = nine;
+    tree.root.left = five;
+    tree.root.right = eleven;
+    tree.root.left = four;
+    expect(tree.breadthFirst([2, 7, 5, 2, 6, 9, 5, 11, 4])).tobe
+  });
+  it('can successfully return value of the maximum node value', () => {
+    let tree = new BT();
+    tree.root = two;
+    tree.root.right = five;
+    tree.root.left = seven;
+    tree.root.left = two;
+    tree.root.right = six;
+    tree.root.right = nine;
+    tree.root.left = five;
+    tree.root.right = eleven;
+    tree.root.left = four;
+    expect(tree.findMaxValue([11])).tobe
+  });
 })
---
+++ code-challenges/Data-Structures/tree/tree.js
@@ -0,0 +1,122 @@
+'use strict';
+debugger;
+class Node {
+  
+  constructor (val) {
+    this.value = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+class BinaryTree {
+  
+  constructor() {
+    this.root = null;
+  }
+  insert(val) {
+  }
+  preOrder() {
+    let results = [];
+    let _walk = (node) => {
+      results.push(node.value);
+      if (node.left) { _walk(node.left); }
+      if (node.right) { _walk(node.right); }
+    };
+    _walk(this.root);
+    return results;
+  }
+  postOrder() {
+    let results = [];
+    let _walk = (node) => {
+      if (node.left) { _walk(node.left); }
+      if (node.right) { _walk(node.right); }
+      results.push(node.value);
+    };
+    _walk(this.root);
+    return results;
+  }
+  inOrder(){
+    let results = [];
+    let _walk = (node) => {
+      if (node.left) { _walk(node.left); }
+      results.push(node.value);
+      if (node.right) { _walk(node.right); }
+    };
+    _walk(this.root);
+    return results;
+  }
+  breadthFirst() {
+    let results = [];
+    let queue = [];
+    queue.push(this.root);
+    while(queue.length) {
+      let current = queue.shift();
+      results.push(current.value);
+      if (current.left) {queue.push(current.left)}
+      if (current.right) {queue.push(current.right)}
+    };
+    return results;
+  }
+  maxVal() {
+    if (this.root === null) {
+      throw 'Empty tree!';
+    }
+    let currNode = this.root;
+    while (currNode.right !== null) {
+      currNode = currNode.right;
+    }
+    return currNode.value;
+  }
+}
+class BinarySearchTree extends BinaryTree {
+  constructor (root = null) {
+    super();
+    this.root = root;
+  }
+  add(value) {
+    let current = this.root;
+    if(!current){this.root = new Node(value)}
+    while (current) {
+      if (current.val > value) {
+        if (!current.left) {
+          current.left = new Node(value);
+          break;
+        }
+        else{current = current.left}
+      }
+      if (current.val < value) {
+        if (!current.right) {
+          current.right = new Node(value);
+          break;
+        }
+        else{ current = current.right}
+      }
+    }
+    return this;
+  }
+  contains(value){
+    return this.preOrder().includes(value);
+  }
+}
+let ten = new Node(10);
+let four = new Node(4);
+let seven = new Node(7);
+let eleven = new Node(11);
+let nine = new Node(9);
+let fifteen = new Node(15);
+let twelve = new Node(12);
+let tree = new BinaryTree();
+tree.root = ten;
+ten.left = seven;
+ten.right = twelve;
+seven.left = four;
+seven.right = nine;
+twelve.left = eleven;
+twelve.right = fifteen;
+console.log(tree);
+console.log('tree max value:', tree.maxVal());
+console.log('tree preOrder :', tree.preOrder());
+console.log('tree postOrder :', tree.postOrder());
+console.log('tree inOrder :', tree.inOrder());
+console.log('Breadth First :', tree.breadthFirst());
+module.exports = { BinarySearchTree, BinaryTree, Node };
