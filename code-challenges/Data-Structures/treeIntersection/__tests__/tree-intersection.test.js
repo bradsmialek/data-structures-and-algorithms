344714cdd5343088c4171ef218315c899d762c95
bjwine0 <bradsmilek@gmail.com>
2019-09-21 02:57:39 -0700

    setup

---
+++ code-challenges/Data-Structures/treeIntersection/__tests__/tree-intersection.test.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const intersect = require('../tree-intersection.js');
+
+//Test should include;
+// “Happy Path” - Expected outcome
+// Expected failure
+// Edge Case (if applicable/obvious)
+
+describe('Test Repeat Word', () => {
+
+  it('should return ', () => {
+    expect(repeat(str)).toEqual('a');
+    expect(repeat(strTwo)).toEqual('was');
+    expect(repeat(strThree)).toEqual('the');
+  });
+
+  it('should not accept ', () => {
+    let string = ;
+    expect(repeat(string)).toBe(undefined);
+  });
+
+  it('should ', () => {
+    let string = ;
+    expect(repeat(string)).toEqual('the');
+  });
+
+});
